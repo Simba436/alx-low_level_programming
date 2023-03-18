2d5e3cf82a2dc442ccfda26632b942ae76312bfe
Simba436 <simbaelton04@gmial.com>
2023-03-18 09:19:06 -0700

    Sasolway

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+	char digit;
+	for(digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	for(digit = 'a'; digit <= 'f'; digit++)
+	{
+		putchar(digit);
+	}
+	
+	putchar('\n');
+
+	return 0;
+}

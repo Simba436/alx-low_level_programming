cf8438184c6fc3498579ee7d07d898c24ee8380a
Simba436 <simbaelton04@gmial.com>
2023-03-21 00:37:22 -0700

    creeping

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Main - Prints out a text
+ *
+ * Description: prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0 (Succesful)
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but thw outcome is a piece of art,\n");
+	return (0);
+}

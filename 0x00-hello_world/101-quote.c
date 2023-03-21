96b91e81ad62b10e9a271d3968273dabe187156f
Simba436 <simbaelton04@gmial.com>
2023-03-21 09:45:10 -0700

    winter

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 1 (Error)
  */
+
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char message[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, message, sizeof(message) - 1);
 	return (1);
 }

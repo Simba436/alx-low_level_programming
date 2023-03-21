4127fa868e4c88a1b569a2d35f7163401fc98951
Simba436 <simbaelton04@gmial.com>
2023-03-21 00:23:20 -0700

    legend

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/*
+ * main prints out a quote
+ *
+ * return is always  0 when succesful
+ */
 
-int main()
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

d2029990c36bdcdf0d42a3b7f65c6a3f77fb8521
Simba436 <simbaelton04@gmial.com>
2023-03-21 10:23:17 -0700

    crazy

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,15 +3,19 @@
 /**
  * main - Entry point
  *
- * Description: crazy stuff
- *
- * Return: Always 0 (success)
- *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("_putchar\n");
-
-	return (0);
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+    return (0);
 }
+

61e818c652806b3b7fc5eb528f0f7fb25218fc51
Simba436 <simbaelton04@gmial.com>
2023-03-21 09:49:31 -0700

    Birds

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,13 @@
  * main - Entry point
  *
  * Return: Always 1 (Error)
+ *
  */
 
 int main(void)
 {
-	char message[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, message, sizeof(message) - 1);
+	char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, x, sizeof(x) - 1);
 	return (1);
 }
 

9a1c4c788d9f6d3fe6c7b0b7edc1f6dd73fd26a1
Simba436 <simbaelton04@gmial.com>
2023-03-22 03:00:08 -0700

    repetition

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /** 
- * print_alphabet - make the alphabet
+ * print_alphabet - Make the alphabet
  *
  * Return:void
  *
  */
-void print_alphabet(void)
-{
+void print_alphabet(void){
 char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
